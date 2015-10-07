048cc02bbdee6d20890165ab85fdf15d0bedbdff
Shyawn Karim <shyawnkarim@yahoo.com>
2015-10-07 09:54:01 -0700

    many Poly class public functions defined

--- Poly.h
+++ Poly.h
@@ -8,8 +8,30 @@
 
 class Poly
 {
+    friend istream& operator>>(istream &inStream, Poly &);
+    friend ostream& operator<<(ostream &outStream, const Poly &);
+
 public:
     Poly();
+    Poly(int);
+    Poly(int, int);
+    Poly(const Poly &);
+    ~Poly();
+
+    int getCoeff() const;
+    void setCoeff(int, int) const;
+
+    Poly operator+(const Poly &) const;
+    Poly operator-(const Poly &) const;
+    Poly operator*(const Poly &) const;
+
+    Poly operator+=(const Poly &);
+    Poly operator-=(const Poly &);
+    Poly operator*=(const Poly &);
+
+    bool operator==(const Poly &) const;
+    bool operator!=(const Poly &) const;
+    void operator=(const Poly &);
 
 private:
 
